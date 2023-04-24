bc3862855fabc121284264f03dc2ff49f0eb3145
Kishor Kumar Paroi <1703053@student.ruet.ac.bd>
2023-04-24 21:43:08 +0600

    done first day project

--- Day-01-Print-Comments/band-name-generator.py
+++ Day-01-Print-Comments/band-name-generator.py
@@ -1,1 +1,4 @@
-print("Welcome to the KKP's World")
+print("Welcome to the KKP's World of Band Name Generator")
+city = input("What's the name of the City you grew up in? \n")
+petName = input("What's your pet's name? \n")
+print("Your Band Name could be " + city + " " + petName)
