dbe13043b7e846452074995a0189633fddcfcb2c
Kishor Kumar Paroi <1703053@student.ruet.ac.bd>
2023-04-24 21:46:20 +0600

    len gets length in intger and sys gets size in bytes

--- Day-01-Print-Comments/1-4-len.py
+++ Day-01-Print-Comments/1-4-len.py
@@ -3,5 +3,5 @@ print("Print the Size of the input String : ")
 string = input()
 print("The size of " + string + " is -> ", len(string))
 
-# sys gets the bit size
+# sys gets the bytes size
 print(sys.getsizeof(string))
