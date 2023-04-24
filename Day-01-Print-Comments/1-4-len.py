1f09566e83210ef4cb0d5b739023fb8301857e16
Kishor Kumar Paroi <1703053@student.ruet.ac.bd>
2023-04-24 21:29:24 +0600

    1-5-variables excercise added

--- Day-01-Print-Comments/1-4-len.py
+++ Day-01-Print-Comments/1-4-len.py
@@ -1,3 +1,6 @@
+import sys
 print("Print the Size of the input String : ")
 string = input()
 print("The size of " + string + " is -> ", len(string))
+
+print(sys.getsizeof(string))
---
+++ Day-01-Print-Comments/1-5-variables.py
@@ -0,0 +1,9 @@
+a = input("Enter first Number : ")
+b = input("Enter Second Number : ")
+
+c = a
+a = b
+b = c
+
+print("a : ", a)
+print("b : ", b)
